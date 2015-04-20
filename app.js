f78560b3acb04abfa7004286c094c58e6632e34c
Kevin Yokley <kyokley2@gmail.com>
2015-04-20 06:54:36 -0500

    Using Node.js

---
+++ app.js
@@ -0,0 +1,91 @@
+
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+var routes = require('./routes/index');
+var users = require('./routes/user');
+
+var app = express();
+
+ 
+
+  app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/views/index.html');
+  console.log(__dirname);
+});
+// view engine setup
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 3001);
+
+var env = process.env.NODE_ENV || 'development';
+app.locals.ENV = env;
+app.locals.ENV_DEVELOPMENT = env == 'development';
+
+// app.use(favicon(__dirname + '/public/img/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/', routes);
+//app.use('/users', users);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err,
+            title: 'error'
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {},
+        title: 'error'
+    });
+});
+ 
+var server = http.createServer(app).listen(3001, function(){
+  console.log("Express server listening on port 3001");
+});
+var io = require("socket.io");
+var ioServer = io.listen(server);
+ioServer.sockets.on('connection', function (socket) {
+  socket.emit("message", "Welcome to Revealer");
+socket.on("slidechanged", function(data){
+  socket.broadcast.emit("slidechanged", data);
+});
+});
+module.exports = app;
