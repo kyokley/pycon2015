2d97b6b7a6d049324c9ffc770e1ee2ea75646769
Kevin Yokley <kyokley2@gmail.com>
2015-04-22 08:59:16 -0500

    Minor changes

--- public/js/socketstuff.js
+++ public/js/socketstuff.js
@@ -1,5 +1,5 @@
 getSocket = function(){
-    var socket = io.connect("http://dev007.texturallc.net:3001");
+    var socket = io.connect("http://dev007-2.texturallc.net:3001");
     socket.on('slidechanged', function (data) {
       Reveal.slide(data.indexh, data.indexv, data.indexf);
     });
